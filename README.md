5962707f5492981c8fef2335a2cda2f3692c6bd8
Chris Oakman <chris@oakmac.com>
2021-02-16 15:38:49 -0600

    add function definitions

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 ## TODO:
 
+- [x] function definitions
 - [ ] set up test suite
+  - use busted? https://olivinelabs.com/busted/
+  - https://github.com/bluebird75/luaunit
 
 ## License
 
--- parinfer.lua
+++ parinfer.lua
@@ -1,1 +1,441 @@
--- TODO: write me :)
+-- parinfer.lua - a Parinfer implementation in Lua
+-- v0.1.0
+-- https://github.com/oakmac/parinfer-lua
+--
+-- More information about Parinfer can be found here:
+-- http://shaunlebron.github.io/parinfer/
+--
+-- Copyright (c) 2021, Chris Oakman and other contributors
+-- Released under the ISC license
+-- https://github.com/oakmac/parinfer-lua/blob/master/LICENSE.md
+
+
+local M = {}
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Constants / Type Predicates
+
+local UINT_NULL = -999
+
+local INDENT_MODE = 'INDENT_MODE'
+local PAREN_MODE = 'PAREN_MODE'
+
+local BACKSLASH = '\\'
+local BLANK_SPACE = ' '
+local DOUBLE_SPACE = '  '
+local DOUBLE_QUOTE = '"'
+local NEWLINE = '\n'
+local TAB = '\t'
+
+-- local LINE_ENDING_REGEX = /\r?\n/
+
+local MATCH_PAREN = {
+  '{': '}',
+  '}': '{',
+  '[': ']',
+  ']': '[',
+  '(': ')',
+  ')': '('
+}
+
+local function isBoolean (x) 
+  -- TODO: write me :)
+end
+
+local function isArray (x)
+  -- TODO: write me :)
+end
+
+local function isInteger (x) 
+  -- TODO: write me :)
+end
+
+local function isString (s) 
+  -- TODO: write me :)
+end
+
+local function isChar (c) 
+  -- TODO: write me :)
+end
+
+local function isArrayOfChars (arr) 
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Options Structure
+
+local function transformChange (change) 
+  -- TODO: write me :)
+end
+
+local function transformChanges (changes)
+  -- TODO: write me :)
+end
+
+local function parseOptions (options) 
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Result Structure
+
+local function initialParenTrail () 
+  -- TODO: write me :)
+end
+
+local function getInitialResult (text, options, mode, smart)
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Possible Errors
+
+
+-- `result.error.name` is set to any of these
+local ERROR_QUOTE_DANGER = 'quote-danger'
+local ERROR_EOL_BACKSLASH = 'eol-backslash'
+local ERROR_UNCLOSED_QUOTE = 'unclosed-quote'
+local ERROR_UNCLOSED_PAREN = 'unclosed-paren'
+local ERROR_UNMATCHED_CLOSE_PAREN = 'unmatched-close-paren'
+local ERROR_UNMATCHED_OPEN_PAREN = 'unmatched-open-paren'
+local ERROR_LEADING_CLOSE_PAREN = 'leading-close-paren'
+local ERROR_UNHANDLED = 'unhandled'
+
+local errorMessages = {}
+errorMessages[ERROR_QUOTE_DANGER] = 'Quotes must balanced inside comment blocks.'
+errorMessages[ERROR_EOL_BACKSLASH] = 'Line cannot end in a hanging backslash.'
+errorMessages[ERROR_UNCLOSED_QUOTE] = 'String is missing a closing quote.'
+errorMessages[ERROR_UNCLOSED_PAREN] = 'Unclosed open-paren.'
+errorMessages[ERROR_UNMATCHED_CLOSE_PAREN] = 'Unmatched close-paren.'
+errorMessages[ERROR_UNMATCHED_OPEN_PAREN] = 'Unmatched open-paren.'
+errorMessages[ERROR_LEADING_CLOSE_PAREN] = 'Line cannot lead with a close-paren.'
+errorMessages[ERROR_UNHANDLED] = 'Unhandled error.'
+
+local function cacheErrorPos (result, errorName) 
+  -- TODO: write me :)
+end
+
+local function error2 (result, errorName)
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- String Operations
+
+local function replaceWithinString (orig, startIdx, endIdx, replace) 
+  -- TODO: write me :)
+end
+
+local function repeatString (text, n)
+  -- TODO: write me :)
+end
+
+local function getLineEnding (text) 
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Line Operations
+
+local function isCursorAffected (result, start, end2) 
+  -- TODO: write me :)
+end
+
+local function shiftCursorOnEdit (result, lineNo, start, end2, replace)
+  -- TODO: write me :)
+end
+
+local function replaceWithinLin (result, lineNo, start, end2, replace) 
+  -- TODO: write me :)
+end
+
+local function insertWithinLine (result, lineNo, idx, insert)
+  -- TODO: write me :)
+end
+
+local function initLine (result)
+  -- TODO: write me :)
+end
+
+local function commitChar (result, origCh)
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Misc Util
+
+local function clamp (val, minN, maxN) 
+  -- TODO: write me :)
+end
+
+local function peek (arr, idxFromBack)
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Character Predicates
+
+local function isOpenParen (ch) 
+  -- TODO: write me :)
+end
+
+local function isCloseParen (ch)
+  -- TODO: write me :)
+end
+
+local function isValidCloseParen (parenStack, ch) 
+  -- TODO: write me :)
+end
+
+local function isWhitespace (result)
+  -- TODO: write me :)
+end
+
+local function isClosable (result) 
+  -- TODO: write me :)
+end
+
+local function isCommentChar (ch, commentChars)
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Advanced Character Operations
+
+
+local function checkCursorHolding (result) 
+  -- TODO: write me :)
+end
+
+local function trackArgTabStop (result, state)
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Literal Character Events
+
+
+local function onOpenParen (result) 
+  -- TODO: write me :)
+end
+
+local function setCloser (opener, lineNo, x, ch)
+  -- TODO: write me :)
+end
+
+local function onMatchedCloseParen (result) 
+  -- TODO: write me :)
+end
+
+local function onUnmatchedCloseParen (result)
+  -- TODO: write me :)
+end
+
+local function onCloseParen (result) 
+  -- TODO: write me :)
+end
+
+local function onTab (result)
+  -- TODO: write me :)
+end
+
+local function onCommentChar (result)
+  -- TODO: write me :)
+end
+
+local function onNewline (result)
+  -- TODO: write me :)
+end
+
+local function onQuote (result) 
+  -- TODO: write me :)
+end
+
+local function onBackslash (result)
+  -- TODO: write me :)
+end
+
+local function afterBackslash (result) 
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Character Dispatch
+
+local function onChar (result)
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Cursor Functions
+
+local function isCursorLeftOf(cursorX, cursorLine, x, lineNo) 
+  -- TODO: write me :)
+end
+
+local function isCursorRightOf (cursorX, cursorLine, x, lineNo)
+  -- TODO: write me :)
+end
+
+local function isCursorInComment (result, cursorX, cursorLine) 
+  -- TODO: write me :)
+end
+
+local function handleChangeDelta (result) 
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Paren Trail Functions
+
+local function resetParenTrail(result, lineNo, x) 
+  -- TODO: write me :)
+end
+
+local function isCursorClampingParenTrail (result, cursorX, cursorLine)
+  -- TODO: write me :)
+end
+
+local function clampParenTrailToCursor (result) 
+  -- TODO: write me :)
+end
+
+local function popParenTrail (result) 
+  -- TODO: write me :)
+end
+
+local function getParentOpenerIndex (result, indentX)
+  -- TODO: write me :)
+end
+
+local function correctParenTrail (result, indentX)
+  -- TODO: write me :)
+end
+
+local function cleanParenTrail (result)
+  -- TODO: write me :)
+end
+
+local function appendParenTrail (result)
+  -- TODO: write me :)
+end
+
+local function invalidateParenTrail (result)
+  -- TODO: write me :)
+end
+
+local function checkUnmatchedOutsideParenTrail (result)
+  -- TODO: write me :)
+end
+
+local function setMaxIndent (result, opener)
+  -- TODO: write me :)
+end
+
+local function rememberParenTrail (result)
+  -- TODO: write me :)
+end
+
+local function updateRememberedParenTrail (result)
+  -- TODO: write me :)
+end
+
+local function finishNewParenTrail (result)
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Indentation Functions
+
+local function addIndent (result, delta)
+  -- TODO: write me :)
+end
+
+local function shouldAddOpenerIndent (result, opener)
+  -- TODO: write me :)
+end
+
+local function correctIndent (result)
+  -- TODO: write me :)
+end
+
+local function onIndent (result)
+  -- TODO: write me :)
+end
+
+local function checkLeadingCloseParen (result)
+  -- TODO: write me :)
+end
+
+local function onLeadingCloseParen (result)
+  -- TODO: write me :)
+end
+
+local function onCommentLine (result)
+  -- TODO: write me :)
+end
+
+local function checkIndent (result)
+  -- TODO: write me :)
+end
+
+local function makeTabStop (result, opener)
+  -- TODO: write me :)
+end
+
+local function getTabStopLine (result)
+  -- TODO: write me :)
+end
+
+local function setTabStops (result)
+  -- TODO: write me :)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- High-level processing functions
+
+local function processChar (result, ch)
+  -- TODO: write me :)
+end
+
+local function processLine (result, lineNo)
+  -- TODO: write me :)
+end
+
+local function finalizeResult (result)
+  -- TODO: write me :)
+end
+
+local function processError (result, e)
+  -- TODO: write me :)
+end
+
+local function processText (text, options, mode, smart)
+  -- TODO: write me 
+return M:)
+end
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Public API
+
+local function publicResult (result)
+  -- TODO: write me :)
+end
+
+local function indentMode (text, options)
+  -- TODO: write me :)
+end
+
+local function parenMode (text, options)
+  -- TODO: write me :)
+end
+
+local function smartMode (text, options)
+  -- TODO: write me :)
+end
+
+M.version = '0.1.0'
+M.indentMode = indentMode
+M.parentMode = parentMode
+M.smartMode = smartMode
